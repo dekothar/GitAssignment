94b5097993a70b5b40c591eae3e2ce5b52ef492e
dekothar <159803137+dekothar@users.noreply.github.com>
2024-11-24 12:34:06 +0530

    Create SubtractorTest.java

---
+++ test/SubtractorTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import src.Subtractor;
+
+public class SubtractorTest {
+	public void testSubtract() {
+	     Subtractor subtractor = new Subtractor();
+	     int result = subtractor.substract(5,2);
+	     assert(result==3);
+	   }
+}
