88166e85f1697dc36052453cd9130a3112ca5029
dekothar <159803137+dekothar@users.noreply.github.com>
2024-11-24 12:43:51 +0530

    Added Subtractor and SubtractorTest

--- test/SubtractorTest.java
+++ test/SubtractorTest.java
@@ -6,6 +6,7 @@ public class SubtractorTest {
 	public void testSubtract() {
 	     Subtractor subtractor = new Subtractor();
 	     int result = subtractor.substract(5,2);
-	     assert(result==3);
+	     
+		assert(result==3);
 	   }
 }
