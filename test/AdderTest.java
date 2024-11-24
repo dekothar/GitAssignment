bd8013ecd4d803189cd4d6e2c0adaef7d1d2d45d
dekothar <159803137+dekothar@users.noreply.github.com>
2024-11-24 12:30:53 +0530

    Create AdderTest.java

---
+++ test/AdderTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import src.Adder;
+
+public class AdderTest {
+	
+		   public void testAdd() {
+		     Adder adder = new Adder();
+		     int result = adder.add(1,2);
+		     assert(result==3);
+		   }
+}
